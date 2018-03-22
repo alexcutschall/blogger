315d3b9a6938a54e3498bf2807786e8d477e25fc
alexcutschall <alexander.cutschall@gmail.com>
2018-03-22 15:48:07 -0600

    Adding on create function

---
+++ spec/features/user_can_create_a_new_article_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'user sees the create a new article form' do
+  describe 'they link from the articles index' do
+    it 'creates a new article when form is submitted' do
+      article_1 = Article.create!(title: "Why Hip Hop Matters", body: "Because it's awesome")
+
+      visit articles_path
+      click_link("Create a New Article")
+
+      expect(current_path).to eq(new_article_path)
+
+      fill_in "article[title]", with: "The Incredibles"
+      fill_in "article[body]", with: "One of the best animated films"
+      click_on "Create Article"
+
+      expect(page).to have_content("The Incredibles")
+      expect(page).to have_content("One of the best animated films")
+    end
+  end
+end
