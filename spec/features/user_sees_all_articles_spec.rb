6ea30502fb0d52d2aa9c6783c4863c2264200068
alexcutschall <alexander.cutschall@gmail.com>
2018-03-22 13:45:47 -0600

    Adding article test

---
+++ spec/features/user_sees_all_articles_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+describe "user sees all articles" do
+  describe "they visit /articles" do
+    it "displays all articles" do
+    article_1 = Article.create!(title: "Why Hip Hop Matters", body: "Because it's awesome")
+    article_2 = Article.create!(title: "Why Jazz Matters", body: "Because of it's unbridled expression")
+    visit '/articles'
+
+    expect(page).to have_content(article_1.title)
+    expect(page).to have_content(article_1.body)
+    expect(page).to have_content(article_2.title)
+    expect(page).to have_content(article_2.body)
+    end
+  end
+end
