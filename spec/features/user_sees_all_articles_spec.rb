d396005dd6e6e5269d891d28f7bce7bd01b0fee4
alexcutschall <alexander.cutschall@gmail.com>
2018-03-22 14:37:04 -0600

    added in link to specific article and new article

--- spec/features/user_sees_all_articles_spec.rb
+++ spec/features/user_sees_all_articles_spec.rb
@@ -4,10 +4,12 @@ describe "user sees all articles" do
     it "displays all articles" do
     article_1 = Article.create!(title: "Why Hip Hop Matters", body: "Because it's awesome")
     article_2 = Article.create!(title: "Why Jazz Matters", body: "Because of it's unbridled expression")
+
     visit '/articles'
 
-    expect(page).to have_content(article_1.title)
-    expect(page).to have_content(article_2.title)
+    expect(page).to have_link(article_1.title)
+    expect(page).to have_link(article_2.title)
+    expect(page).to have_link("Create a New Article")
     end
   end
 end
